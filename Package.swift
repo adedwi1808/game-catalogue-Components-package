ea30acf9968534e471fd13f2c34f22f5123196bb
Ade Dwi Prayitno <adedwip1808@gmail.com>
2026-01-16 23:01:59 +0700

    Initial release 1.0.0

--- Package.swift
+++ Package.swift
@@ -5,19 +5,27 @@ import PackageDescription
 
 let package = Package(
     name: "Components",
+    platforms: [.iOS(.v15)],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
-        .library(
-            name: "Components",
-            targets: ["Components"]
+        .library(name: "Components", targets: ["Components"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/adedwi1808/game-catalogue-Core-package.git", from: "1.0.0"),
+.package(url: "https://github.com/adedwi1808/game-catalogue-Common-package.git", from: "1.0.0"),
+
+        .package(
+            url: "https://github.com/onevcat/Kingfisher.git",
+            from: "8.0.0"
+        ),
+        .package(
+            url: "https://github.com/Juanpe/SkeletonView.git",
+            from: "1.7.0"
         ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Components"
-        ),
-
+            name: "Components",
+            dependencies: ["Core", "Common", "Kingfisher", "SkeletonView"]
+        )
     ]
 )
